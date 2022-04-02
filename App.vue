7abdba19a2ac5f3026bc11fb8c3a5d12c4465c45
少玲 <shaoling.zheng@zyd.cn>
2022-04-02 14:50:46 +0800

    新增mp-extend

--- App.vue
+++ App.vue
@@ -1,7 +1,11 @@
 <script>
+	// 定义 全局混入
+	import './utils/mp-extend/index'
 	export default {
 		globalData: {
-				toastDuration: 2000
+				toastDuration: 2000,
+				userInfo: {},
+				imgUrl: "https://zydmall-tinyapp.oss-cn-shenzhen.aliyuncs.com/miniapp/images",
 		},
 		onLaunch: function() {
 			console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
---
+++ utils/mp-extend/index.js
@@ -0,0 +1,16 @@
+import MpExtend from  './extend.js'
+
+import extendGlobalData from './module/globalData.js'
+MpExtend(extendGlobalData)
+
+
+App = MpExtend.App
+Page = MpExtend.Page
+Component = MpExtend.Component
+
+// 暴露App、Page、Component构造函数
+export default {
+  App: mpExtend.App,
+  Page: mpExtend.Page,
+  Component: mpExtend.Component,
+};
